b4cc6b9e9124e5ce15a204a7ecdf46002bfd8dad
whittcarson <131502055+whittcarson@users.noreply.github.com>
2023-04-21 21:53:35 -0400

    Changed Application Name
    I changed the name of the Application in the UI file to "Carson's Version Control Assignment.

--- src/main/java/com/houarizegai/calculator/ui/CalculatorUI.java
+++ src/main/java/com/houarizegai/calculator/ui/CalculatorUI.java
@@ -17,7 +17,7 @@ public class CalculatorUI {
 
     private static final String FONT_NAME = "Comic Sans MS";
     private static final String DOUBLE_OR_NUMBER_REGEX = "([-]?\\d+[.]\\d*)|(\\d+)|(-\\d+)";
-    private static final String APPLICATION_TITLE = "Calculator";
+    private static final String APPLICATION_TITLE = "Carson's Version Control Assignment";
     private static final int WINDOW_WIDTH = 410;
     private static final int WINDOW_HEIGHT = 600;
     private static final int BUTTON_WIDTH = 80;
